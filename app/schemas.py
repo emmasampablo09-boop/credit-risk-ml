4917881ac075ff37437b736a30aaa756ee5234e9
sampablo <emmasampablo09@gmail.com>
2026-03-10 10:51:29 -0600

    Refactor ClienteInput schema fields

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,11 @@
 from pydantic import BaseModel, Field
 
 class ClienteInput(BaseModel):
-    ExternalRiskEstimate: float = Field(..., ge = 0, le=100)
-    AverageMInFile: float = Field(..., ge = 0)
-    MSinceOldestTradeOpen: float = Field(..., ge = 0)
-    NumSatisfactoryTrades: float = Field(..., ge = 0)
-    PercentTradesNeverDelq: float = Field(..., ge = 0, le=100)
-    MSinceMostRecentInqexcl7days: float = Field(..., ge = 0)
-    MaxDelq2PublicRecLast12M: float = Field(..., ge = 0)
-    MaxDelqEver: float = Field(..., ge = 0)
-    
+    NetFractionRevolvingBurden:      float = Field(..., ge=0, le=100)
+    ExternalRiskEstimate:            float = Field(..., ge=0, le=100)
+    AverageMInFile:                  float = Field(..., ge=0)
+    PercentTradesWBalance:           float = Field(..., ge=0, le=100)
+    MSinceOldestTradeOpen:           float = Field(..., ge=0)
+    NumSatisfactoryTrades:           float = Field(..., ge=0)
+    PercentInstallTrades:            float = Field(..., ge=0, le=100)
+    MSinceMostRecentInqexcl7days:    float = Field(..., ge=0)
